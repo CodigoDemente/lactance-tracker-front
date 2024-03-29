4788e2590f88579c07d56a03e6b25d2c4de1ddaf
Edith <eddith@proton.me>
2024-03-29 15:58:10 +0100

    feat: home refactor and Board container

---
+++ src/containers/Board.jsx
@@ -0,0 +1,211 @@
+import React, { useState } from 'react';
+import check from '../assets/ellipse.svg';
+import { FaEdit } from "react-icons/fa";
+import { IoSend } from "react-icons/io5";
+import { Link,  } from "react-router-dom";
+import styled from 'styled-components';
+import Button from '../components/Button';
+import Input from '../components/Input';
+import { colors } from '../styles/colors';
+import Tabs from '../components/Tabs';
+import { parseTimeToISO } from '../utils/parserTime';
+import Paragraph from '../components/Paragraph';
+
+const buttons = [
+  {
+    name: 'breast',
+    text: '🤱 Tits',
+  },
+  {
+    name: 'bottle',
+    text: '🍼 Bibs'
+  }
+]
+
+const inputs = [
+  {
+    name: 'hours',
+    placeholder: "Hours..."
+  },
+  {
+    name: 'minutes',
+    placeholder: 'Minutes...'
+  }
+]
+
+const radio = [
+  {
+    name: 'breast'
+  },
+  {
+    name: 'bottle'
+  }
+]
+
+const Board = ({
+    apiPostMeal,
+    userId,
+    childs,
+    submittedForm,
+    tab,
+    onChangeTab
+}) => {
+
+  
+  const [edit, setEdit] = useState(false);
+  const [errorForm, setErrorForm] = useState('')
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    apiPostMeal(e.target.name, tab);
+  }
+
+  const registerManually = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const { hours, minutes, radio } = Object.fromEntries(formData.entries());
+    
+    if (hours.length !== 2 || minutes.length !== 2 || isNaN(hours) || isNaN(minutes) || hours >= 24 || minutes >= 60) {
+      setErrorForm('Time format should be 2 digits and valid time')
+    }
+    else if (!radio) {
+      setErrorForm('You should select a type of meal')
+    } else {
+      const isoString = parseTimeToISO(hours, minutes)
+      apiPostMeal(radio, tab, isoString);
+    }
+  }
+
+  
+  return (
+    <>
+      <FlexContainer>
+        <Tabs
+          tabs={childs}
+          onChangeTab={onChangeTab}
+          activeTab={tab}
+        />
+      </FlexContainer>
+      <CheckContainer>{submittedForm && <img src={check} alt="check" />}</CheckContainer>
+      {childs[tab] && (
+        <FlexContainer $gap={'40px'} $col>
+          <StyledLink to={`${userId}/child/${childs[tab].id}`}>
+            {childs[tab].name}
+          </StyledLink>
+          <FlexContainer $col>
+            {buttons.map((button, i) => (
+                <Button
+                    key={i}
+                primary={i % 2 === 0}
+                secondary={i % 2 !== 0}
+                onClick={handleSubmit}
+                name={button.name}>
+                {button.text}
+              </Button>
+            ))}
+          </FlexContainer>
+          <FlexContainer $col>
+            <EditButton onClick={()=> setEdit(!edit)}>
+              Register <FaEdit size={20}/>
+            </EditButton>
+            {edit && <StyledForm onSubmit={registerManually}>
+               <FlexContainer  $gap={'8px'}>
+              {inputs.map((input, i) => (
+                  <Input
+                      label=""
+                      key={i} 
+                    name={input.name} 
+                    placeholder={input.placeholder} 
+                    maxLength="2"
+                    minLength="1"
+                    required
+                    size="small"
+                />
+              ))}
+              </FlexContainer>
+              <FlexContainer $gap={'8px'}>
+              {radio.map((radio, i) => (
+                <div key={i}>
+                  <input id={radio.name} value={radio.name} name="radio" type="radio"/>
+                    <label htmlFor={radio.name} class="radio-label">{radio.name}</label>
+                  </div>
+              ))}
+                <SendButton type="submit">
+                <IoSend size={15} />
+              </SendButton>
+              </FlexContainer>
+              {errorForm && <Paragraph size='s' color='red'>{errorForm}</Paragraph>}
+            </StyledForm>
+            }
+          </FlexContainer>
+        </FlexContainer>
+        )}
+    </>
+  );
+}
+
+
+const FlexContainer = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: ${props => props.$col ? 'column' : 'row'};
+    align-items: center;
+    justify-content: center;
+    gap: ${props => props.$gap || '0px'};
+`
+
+const CheckContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 80px;
+`;
+
+  const StyledLink = styled(Link)`
+    color: ${colors.darkgrey};
+    font-size:  28px;
+    text-decoration: none;
+    font-weight: 600;
+    &:hover {
+        text-decoration: underline;
+    }
+`;
+
+const buttonStyles = `
+    background-color: transparent;
+    border: none;
+    color: ${colors.darkgrey};
+    text-decoration: none;
+    unset: none;
+    cursor: pointer;
+     &:hover {
+        opacity: 0.8;
+    };
+    display: flex;
+    align-items: center;
+    gap: 8px;
+  `;
+
+const EditButton = styled.button`
+   
+    padding-bottom: 20px;
+    ${buttonStyles}
+  `; 
+
+const SendButton = styled.button`
+   ${buttonStyles}
+   color: ${colors.primary};
+  `;
+
+  const StyledForm = styled.form`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 20px;
+`;
+
+
+export default Board;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,173 +1,106 @@
-import React, { useContext, useEffect, useState } from 'react';
-import check from '../assets/ellipse.svg';
-import { FaEdit } from "react-icons/fa";
-import { IoSend } from "react-icons/io5";
-import { Link, useNavigate } from "react-router-dom";
+import React, { useContext, useEffect, useState, useCallback } from 'react';
+import {  useNavigate } from "react-router-dom";
 import styled from 'styled-components';
-import Button from '../components/Button';
-import Input from '../components/Input';
-import { colors } from '../styles/colors';
 import UserContext from '../hooks/UserContext';
-import Paragraph from '../components/Paragraph';
 import Cookies from 'js-cookie';
-
+import { get_childs, post_meal } from '../api/childs';
+import { parseToken } from '../utils/parserToken';
+import Board from '../containers/Board';
+import Error from '../containers/Error';
 
 const Home = () => {
 
-  const [tab, setTab] = useState(0);
-  const [edit, setEdit] = useState(false);
-  const [submittedForm, setSubmittedForm] = useState(false);
-  const [errorAPICall, setErrorAPICall] = useState(false);
-  const [childs, setChilds] = useState(null);
-  
   const token = Cookies.get('token')
 
-  const { user } = useContext(UserContext);
+  const [tab, setTab] = useState(0);
+  const { userId, setUser } = useContext(UserContext);
   const navigate = useNavigate();
-
-  const getChilds = async () => {
-    const settings = {
-      method: 'GET',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${ token }`
-            },
-        };
-        try {
-            const response = await fetch(`https://lactance-tracker-back-dev-frat.2.ie-1.fl0.io/parents/${user}/childs`, settings)
-          if (response.status === 200) {
-            const body = await response.json();
-            setChilds(body)
-            }
-        } catch (e) {
-            setErrorAPICall(true)
-            return e;
-        }
-  }
+  const [loading, setLoading] = useState(false)
+  const [errorAPICall, setErrorAPICall] = useState('');
   
-  const postMeal = async (type) => {
-    const settings = {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${token}`
-      },
-      body: JSON.stringify({
-        type: type
-      })
-    };
-    try {
-      const response = await fetch(`https://lactance-tracker-back-dev-frat.2.ie-1.fl0.io/childs/${childs[tab].id}/meals`, settings)
-      if (response.status === 201) {
-        setSubmittedForm(true)
-      }
-    } catch (e) {
-      setErrorAPICall(true)
-      return e;
-    }
-  }
+  const [submittedForm, setSubmittedForm] = useState(false);
+  const [childs, setChilds] = useState(null);
 
-  useEffect(() => {
-    if (!user) {
+
+  const onChangeTab = (e) => {
+    setTab(e.target.value);
+  }
+  const apiPostMeal = useCallback(async (type, tab, date = null) => {
+    setLoading(true)
+    const response = await post_meal(childs[tab].id, type, date);
+    if (response) {
+      setSubmittedForm(true);
+      setErrorAPICall(false)
+      setLoading(false)
+      setTimeout(() => {setSubmittedForm(false)}, 800)
+    }
+    else {
+      setErrorAPICall('Something went wrong when registering lactance!')
+      setLoading(false)
+    }
+  }, [childs]);
+ 
+  const apiGetChilds = useCallback(async () => {
+    if (userId) {
+      setLoading(true)
+      const response = await get_childs(userId);
+      if (response) {
+        setChilds(response);
+        setErrorAPICall(false);
+        setLoading(false)
+      }
+      else {
+        setErrorAPICall('Something went wrong when getting your data!')
+        setLoading(false)
+      }
+    }
+  }, [userId]);
+
+
+
+  useEffect(() => { 
+    if (!token) {
       navigate('/login');
     } else {
-      getChilds();
+      if (!userId) {
+        const parsedToken = parseToken(token);
+        setUser(parsedToken.sub);
+      }
+      apiGetChilds();
     }
-  }, [user, navigate]);
+  }, [token, setUser, userId]);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    postMeal(e.target.name);
-  }
-
-  const registerManually = (e) => {
-    e.preventDefault();
-    console.log('register manually');
-     setSubmittedForm(true)
-  }
-  
-  const onChangeTab = (e) => {
-    setTab(e.target.name);
-    setSubmittedForm(false);
-  }
-  if (!childs) {
+  if (loading) {
     return <div>Loading...</div>
   }
+
+  if (errorAPICall) {
+    return (
+      <Error
+        errorMessage={errorAPICall}
+      />
+    )
+  }
+
+  if(!childs || !childs.length) {
+    return (
+      <PageContainer>
+        <div>No data to show</div>
+      </PageContainer>
+    )  
+  }
+
   return (
     <PageContainer>
-      { errorAPICall && 
-                <Paragraph size='s' color='red'>Something went wrong with your login!</Paragraph>
-                }
-         <FlexContainer>
-        {childs.map((child, i) => {
-        return (
-          <Tab
-            key={child.id}
-            name={i}
-            onClick={onChangeTab}
-            $primary={i % 2 === 0}
-            $secondary={i % 2 !== 0}
-          >{child.name}</Tab>
-        )
-      })}
-      </FlexContainer>
-      <CheckContainer>
-          {
-            submittedForm &&  <img src={check} alt="check" />
-          }
-          </CheckContainer>
-      {childs[tab] && (
-        <FlexContainer $gap={'40px'} $col>
-          <StyledLink to={`${user}/child/${childs[tab].id}`}>
-            {childs[tab].name}
-          </StyledLink>
-          <FlexContainer $col>
-            <Button
-              primary
-              name="breast"
-             onClick={handleSubmit}
-            >
-             🤱 Tits
-            </Button>
-            <Button
-              secondary
-              name="bottle"
-             onClick={handleSubmit}
-            >
-               🍼 Bibs
-            </Button>
-          </FlexContainer>
-          <FlexContainer $col>
-            <EditButton onClick={()=> setEdit(!edit)}>
-              Register <FaEdit size={20}/>
-            </EditButton>
-            {edit && <FlexContainer $gap={'10px'}>
-              <Input
-                name="hours"
-                maxLength="2"
-                minLength="1"
-                required
-                size="small"
-                placeholder="Hours..."
-              />
-              <Input
-                name="minutes"
-                maxLength="2"
-                minLength="1"
-                required
-                size="small"
-                placeholder="Minutes..."
-              />
-              <SendButton onClick={registerManually}>
-                <IoSend size={15} />
-              </SendButton>
-            </FlexContainer>
-            }
-          </FlexContainer>
-        </FlexContainer>
-        )}
+      {childs && childs.length &&
+        <Board
+          apiPostMeal={apiPostMeal}
+          childs={childs}
+          userId={userId}
+        submittedForm={submittedForm}
+        tab={tab}
+        onChangeTab={onChangeTab}
+        />}
     </PageContainer>
   );
 }
@@ -179,72 +112,5 @@ const PageContainer = styled.div`
     justify-content: center;
     padding-top: 40px;
  `;
-
-
-const FlexContainer = styled.div`
-    width: 100%;
-    display: flex;
-    flex-direction: ${props => props.$col ? 'column' : 'row'};
-    align-items: center;
-    justify-content: center;
-    gap: ${props => props.$gap || '0px'};
-`
-
-const CheckContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 80px;
-`;
-
-const Tab = styled.button`
-    background-color: ${props => props.$primary ? colors.primary : colors.white};
-    color: ${props => props.$secondary ? colors.primary : colors.white};
-    width: 100%;
-    font-size: 16px;
-    text-decoration: none;
-    font-weight: 600;
-    padding: 6px;
-    border-radius: 2px;
-    border: 2px solid ${colors.primary};
-    &:hover {
-        opacity: 0.8;
-    }
-  `;
-
-  const StyledLink = styled(Link)`
-    color: ${colors.darkgrey};
-    font-size:  28px;
-    text-decoration: none;
-    font-weight: 600;
-    &:hover {
-        text-decoration: underline;
-    }
-`;
-
-const buttonStyles = `
-    background-color: transparent;
-    border: none;
-    color: ${colors.darkgrey};
-    text-decoration: none;
-    unset: none;
-    cursor: pointer;
-     &:hover {
-        opacity: 0.8;
-    }
-  `;
-
-const EditButton = styled.button`
-    display: flex;
-    align-items: center;
-    gap: 8px;
-    padding-bottom: 20px;
-    ${buttonStyles}
-  `; 
-
-const SendButton = styled.button`
-   ${buttonStyles}
-   color: ${colors.primary};
-  `;
 
 export default Home;
