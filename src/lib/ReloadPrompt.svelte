91198a92bcaf4f3643ef3906a904bfec8417d60d
Alejandro Sánchez Medina <asanchez@leadtech.com>
2024-04-12 15:37:14 +0200

    style: :art: apply prettier to format code

--- src/lib/ReloadPrompt.svelte
+++ src/lib/ReloadPrompt.svelte
@@ -1,50 +1,38 @@
 <script lang="ts">
-	import { useRegisterSW } from 'virtual:pwa-register/svelte'
-	const {
-		needRefresh,
-		updateServiceWorker,
-		offlineReady
-	} = useRegisterSW({
+	import { useRegisterSW } from 'virtual:pwa-register/svelte';
+	const { needRefresh, updateServiceWorker, offlineReady } = useRegisterSW({
 		onRegistered(r) {
-		// uncomment following code if you want check for updates
-		// r && setInterval(() => {
-		//    console.log('Checking for sw update')
-		//    r.update()
-		// }, 20000 /* 20s for testing purposes */)
-			console.log(`SW Registered: ${r}`)
+			// uncomment following code if you want check for updates
+			// r && setInterval(() => {
+			//    console.log('Checking for sw update')
+			//    r.update()
+			// }, 20000 /* 20s for testing purposes */)
+			console.log(`SW Registered: ${r}`);
 		},
 		onRegisterError(error) {
-			console.log('SW registration error', error)
-		},
-	})
+			console.log('SW registration error', error);
+		}
+	});
 	const close = () => {
-		offlineReady.set(false)
-		needRefresh.set(false)
-	}
-	$: toast = $offlineReady || $needRefresh
+		offlineReady.set(false);
+		needRefresh.set(false);
+	};
+	$: toast = $offlineReady || $needRefresh;
 </script>
 
 {#if toast}
 	<div class="pwa-toast" role="alert">
 		<div class="message">
 			{#if $offlineReady}
-				<span>
-					App ready to work offline
-				</span>
+				<span> App ready to work offline </span>
 			{:else}
-				<span>
-					New content available, click on reload button to update.
-				</span>
+				<span> New content available, click on reload button to update. </span>
 			{/if}
 		</div>
 		{#if $needRefresh}
-			<button on:click={() => updateServiceWorker(true)}>
-				Reload
-			</button>
+			<button on:click={() => updateServiceWorker(true)}> Reload </button>
 		{/if}
-		<button on:click={close}>
-			Close
-		</button>
+		<button on:click={close}> Close </button>
 	</div>
 {/if}
 
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,25 +2,25 @@
 	import { onMount } from 'svelte';
 	import { pwaInfo } from 'virtual:pwa-info';
 
-    onMount(async () => {
-    if (pwaInfo) {
-      const { registerSW } = await import('virtual:pwa-register')
-      registerSW({
-        immediate: true,
-        onRegistered(r) {
-          // uncomment following code if you want check for updates
-          // r && setInterval(() => {
-          //    console.log('Checking for sw update')
-          //    r.update()
-          // }, 20000 /* 20s for testing purposes */)
-          console.log(`SW Registered: ${r}`)
-        },
-        onRegisterError(error) {
-          console.log('SW registration error', error)
-        }
-      })
-    }
-  })
+	onMount(async () => {
+		if (pwaInfo) {
+			const { registerSW } = await import('virtual:pwa-register');
+			registerSW({
+				immediate: true,
+				onRegistered(r) {
+					// uncomment following code if you want check for updates
+					// r && setInterval(() => {
+					//    console.log('Checking for sw update')
+					//    r.update()
+					// }, 20000 /* 20s for testing purposes */)
+					console.log(`SW Registered: ${r}`);
+				},
+				onRegisterError(error) {
+					console.log('SW registration error', error);
+				}
+			});
+		}
+	});
 
 	$: webManifestLink = pwaInfo ? pwaInfo.webManifest.linkTag : '';
 </script>
@@ -29,8 +29,8 @@
 	{@html webManifestLink}
 </svelte:head>
 
-<slot></slot>
+<slot />
 
-{#await import('$lib/ReloadPrompt.svelte') then { default: ReloadPrompt}}
-  <ReloadPrompt />
+{#await import('$lib/ReloadPrompt.svelte') then { default: ReloadPrompt }}
+	<ReloadPrompt />
 {/await}
